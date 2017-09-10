fd31dae2fb3d675dec6eb74023b2074ab04a95c3
sforce-dev <cebrian.merari@gmail.com>
2017-09-11 00:32:15 +0800

    add ETSoapConnection, and client properties default values

--- src/main/java/com/magis/web/Login.java
+++ src/main/java/com/magis/web/Login.java
@@ -1,29 +1,28 @@
 package com.magis.web;
 
-import com.exacttarget.fuelsdk.ETClient;
-import com.exacttarget.fuelsdk.ETConfiguration;
-import com.exacttarget.fuelsdk.ETSdkException;
+import com.exacttarget.fuelsdk.*;
 
 public class Login {
-
-
-
+    private static final String DEFAULT_clientId ="29y2z4p6u51s160uk3z9b4cp";
+    private static final String DEFAULT_clientSecret ="Q8AVYav7ICQdl22YyCth3vRj";
+    private static final String DEFAULT_soapEndPoint ="https://webservice.s4.exacttarget.com/Service.asmx";
     public Login() {
+        
         try{
             int stack = 4;
             String soapEndpoint = "";
             ETConfiguration configuration = new ETConfiguration("/fuelsdk.properties");
 
             //set username
-            configuration.set("username","");
+            configuration.set("username","la-mcebrian");
             //set password
-            configuration.set("password","");
+            configuration.set("password","James1:1");
             //set client id
-            configuration.set("clientId","29y2z4p6u51s160uk3z9b4cp");
+            configuration.set("clientId",DEFAULT_clientId);
             //set client secret
-            configuration.set("clientSecret","Q8AVYav7ICQdl22YyCth3vRj");
+            configuration.set("clientSecret",DEFAULT_clientSecret);
             //set soapEndpoint
-
+            
             switch (stack) {
                 case 1:  soapEndpoint = configuration.get("s1_soapEndpoint");
                     break;
@@ -33,18 +32,22 @@ public class Login {
                     break;
                 case 7:  soapEndpoint = configuration.get("s7_soapEndpoint");
                     break;
-                default: soapEndpoint = "Invalid stack";
+                default: soapEndpoint = DEFAULT_soapEndPoint;
                     break;
             }
 
             configuration.set("soapEndpoint",soapEndpoint);
             ETClient client = new ETClient(configuration);
+            ETSoapConnection soap = new ETSoapConnection(client, configuration.get("s4_soapEndpoint"),
+                            configuration.get("username"),
+                            configuration.get("password"));
 
-            System.out.println("Client to String");
-            System.out.println(client.toString());
+            System.out.println("soap to String");
+            System.out.println(soap.getSoap().toString());
         } catch (ETSdkException e){
-            System.out.println(e);
+            System.out.println("ETSdkException to String");
+            System.out.println(e.toString());
         }
 
-    }
+    }    
 }
